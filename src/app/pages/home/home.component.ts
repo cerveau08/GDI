4030a6e728698922aa0c2c8d9367299c21697c28
cerveau08 <malickcoly342@gmail.com>
2021-02-24 08:52:10 +0000

    home interi

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public restant: any;
+  public nombre = 99;
+  public left: any;
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  getwidth() {
+    this.restant = this.nombre + "%";
+    return this.restant;
+  }
+  getposition() {
+    let left1 = this.nombre - 2;
+    this.left = left1 + "%";
+    return this.left;
+  }
 }
