c29c13a44b8ef2274ba7981678a95f2f887d051a
thierno23 <thiernoamadouguiro@gmail.com>
2021-06-18 07:56:55 +0000

    details menager: done

--- src/app/pages/manager/manager.component.ts
+++ src/app/pages/manager/manager.component.ts
@@ -40,6 +40,7 @@ export class ManagerComponent implements OnInit {
       this.activeroute.queryParams.subscribe(params => {
         this.item = JSON.parse(params["user"]);
         console.log(this.item);
+        
         this.otherService.getDetailsManagerById(this.item).subscribe(
           result => {
             this.data = result
@@ -64,7 +65,7 @@ export class ManagerComponent implements OnInit {
      
   }
 
-  openModal(id: string) {g
+  openModal(id: string) {
     this.modalService.open(id);
   }
 
