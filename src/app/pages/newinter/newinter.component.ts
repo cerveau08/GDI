6d4c91c0e4c8388c2c648ea6bfc0d2d5cd39f849
thierno23 <thiernoamadouguiro@gmail.com>
2021-07-15 10:53:53 +0000

    voir plus & finContrat/nouveauxRecrus

--- src/app/pages/newinter/newinter.component.ts
+++ src/app/pages/newinter/newinter.component.ts
@@ -2,6 +2,7 @@ import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/service/data.service';
+import { OthersService } from 'src/app/services/others.service';
 
 @Component({
   selector: 'app-newinter',
@@ -9,17 +10,33 @@ import { DataService } from 'src/app/service/data.service';
   styleUrls: ['./newinter.component.scss']
 })
 export class NewinterComponent implements OnInit {
-
+  dataInterFin;
   public datas: any;
   pager: any = {};
+  nouveauxRrecrus;
   filterterm: string;
   public p: any;
   pagedItems: any[];
   date: any;
-  constructor(private dataService: DataService,
+  constructor(private dataService: DataService,private otherService: OthersService,
     public datepipe: DatePipe,
-    public router: Router
-    ) { }
+    public router: Router){
+
+      this.otherService.getInter().subscribe(
+        data => {
+         this.dataInterFin = data.data;
+         console.log(data);
+        }
+      );
+
+      this.otherService.getNouveauRecrus().subscribe(
+        data => {
+         this.nouveauxRrecrus = data.data;
+         console.log(data);
+        }
+      );
+
+     }
 
   ngOnInit() {
     this.datas = this.dataService.getData();
