cc352b4caf46cbe426e10e40a68df3e77875b047
Malick COLY [SNT DST/DD/DASI/PMA] <malickcoly342@gmail.com>
2021-07-19 11:01:01 +0000

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -204,7 +204,7 @@ const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full'
   },
-  { path: '**', redirectTo: 'login' },
+//  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
