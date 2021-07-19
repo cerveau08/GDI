1dcc32827323ca4fffe0e5334ad6af4e8e7879a7
Malick COLY [SNT DST/DD/DASI/PMA] <malick.coly1@orange-sonatel.com>
2021-07-19 15:17:07 +0000

    attestation ok

--- src/app/pages/attestationpresence/attestationpresence.component.ts
+++ src/app/pages/attestationpresence/attestationpresence.component.ts
@@ -11,11 +11,14 @@ import { environment } from 'src/environments/environment';
 })
 export class AttestationpresenceComponent implements OnInit {
 
+  checkedList:any;
+  selectedAll: any;
+  result;
   datas: any;
   filterterm: string;
   dataAttest: any;
   page = 1;
-  itemsPerPage = 3;
+  itemsPerPage = 10;
   totalItems : any;
   public reqUrl = environment.base_url;
   constructor(private dataService: DataService,
@@ -27,12 +30,36 @@ export class AttestationpresenceComponent implements OnInit {
   this.gty(this.page);
   }
   gty(page: any){
-    this.http.get(this.reqUrl + `/listeAttestationByMonth?page=${page}&size=${this.itemsPerPage}`).subscribe((data: any) => {
-      this.dataAttest =  data.data;
-      this.totalItems = data.total;
-      console.log(this.dataAttest);
-      console.log(this.totalItems);
-      
+    this.http.get(this.reqUrl + `/listeAttestationByMonth?page=${page}&size=${this.itemsPerPage}`).subscribe((data: any) => 
+      this.dataAttest =  data.data[0])
+  }
+  selectAll() {
+    for (var i = 0; i < this.dataAttest.length; i++) {
+      this.dataAttest[i].etat = this.selectedAll;
+    }
+    this.getCheckedItemList();
+  }
+  checkIfAllSelected(event) {
+    this.selectedAll = this.dataAttest.every(function(item:any) {
+      return item.etat == 0;
     })
+    this.getCheckedItemList();
+  }
+  getCheckedItemList() {
+    console.log(this.dataAttest);
+    this.checkedList = [];
+    for (var i = 0; i < this.dataAttest.length; i++) {
+      if(this.dataAttest[i].etat) {
+        this.checkedList.push(this.dataAttest[i]);
+        this.http.post(`${this.reqUrl}/validerAttestation/${this.dataAttest[i].id}`, null).subscribe(
+          data => {
+            this.result = data;
+            console.log(data);
+          }
+        )
+      }
+    }
+    this.checkedList = this.checkedList;
+    console.log(this.checkedList);
   }
 }
