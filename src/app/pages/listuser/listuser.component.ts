1d7bb9b1e442b8b884c5f26b3434358a33029bf1
astaalo12 <mcs221@outlook.com>
2021-07-27 14:56:19 +0000

    listuser

--- src/app/pages/listuser/listuser.component.ts
+++ src/app/pages/listuser/listuser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-listuser',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listuser.component.scss']
 })
 export class ListuserComponent implements OnInit {
-
+  page = 1;
+  itemsPerPage = 7;
+  totalItems : any;
+  filterterm
+  public reqUrl = environment.base_url;
   constructor() { }
 
   ngOnInit() {
