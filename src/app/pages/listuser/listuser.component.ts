ad6664c851ab80af2eb692ea3c0115051a3566dd
Malick COLY [SNT DST/DD/DASI/PMA] <malick.coly1@orange-sonatel.com>
2021-07-27 16:06:25 +0000

    up

--- src/app/pages/listuser/listuser.component.ts
+++ src/app/pages/listuser/listuser.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DataService } from 'src/app/service/data.service';
+import { DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
+import { ModalService } from 'src/app/_modal';
+import { OthersService } from 'src/app/services/others.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-listuser',
@@ -7,14 +14,82 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./listuser.component.scss']
 })
 export class ListuserComponent implements OnInit {
+  public data; any;
+  public datas: any;
+  date: any;
+  role;
+  dataUser: any;
+  attestationForm: FormGroup;
   page = 1;
   itemsPerPage = 7;
   totalItems : any;
   filterterm
   public reqUrl = environment.base_url;
-  constructor() { }
+  result;
+  success;
+  successMsg;
+  constructor(private dataService: DataService,
+    public datepipe: DatePipe,
+    public router: Router,
+    private modalService: ModalService,
+    private otherService: OthersService,
+    private http: HttpClient
+    ) { }
 
   ngOnInit() {
+    this.role = localStorage.getItem('user');
+   // this.getcolor(this.p);
+    
+    this.gty(this.page);
+  }
+
+  
+  gty(page: any){
+    this.http.get(this.reqUrl + `/users/all?page=${page}&limit=${this.itemsPerPage}`).subscribe((data: any) => {
+      this.dataUser =  data.data;
+      this.totalItems = data.total;
+      console.log(data);
+      console.log(this.totalItems);
+    })
+  }
+
+
+  openDetail(data) {
+    this.router.navigate(['/accueil/detailuser'], {
+      queryParams: {
+        user: JSON.stringify(data)
+      }
+    })
+  }
+
+  downloadFile(data: any) {
+    const replacer = (key, value) => (value === null ? '' : value); 
+    const header = Object.keys(data[0]);
+    const csv = data.map((row) =>
+      header
+        .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+        .join(',')
+    );
+    csv.unshift(header.join(','));
+    const csvArray = csv.join('\r\n');
+  
+    const a = document.createElement('a');
+    const blob = new Blob([csvArray], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+  
+    a.href = url;
+    a.download = 'myFile.csv';
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
   }
 
 }
