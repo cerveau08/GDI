5b29b514d0866bf8b4b474c4a0a29b34f54533c2
Malick COLY [SNT DST/DD/DASI/PMA] <malick.coly1@orange-sonatel.com>
2022-01-05 14:21:55 +0000

    all

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  base_url: 'https://backend-dstgestiondesinterimaires-rec.apps.malaaw-rec.orange-sonatel.com'
+  base_url: 'https://gdi-api-dstgestiondesinterimaires-prod.apps.paas.orange-sonatel.com/'
 };
